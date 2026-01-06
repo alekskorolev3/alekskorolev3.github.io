2c3de492883034af15eea6966bcefea83d68aec8
alekskorolev3 <alekskorolev3@gmail.com>
2026-01-06 22:13:18 +0300

    Added GTM

--- src/app/layout.js
+++ src/app/layout.js
@@ -56,6 +56,27 @@ export default function RootLayout({children}) {
             >
 
             </noscript>
+
+
+            <script type="text/javascript"
+                    dangerouslySetInnerHTML={{
+                        __html: `(function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-WHFKJJ4L');`
+                    }}
+            >
+            </script>
+
+            <noscript
+                dangerouslySetInnerHTML={{
+                    __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WHFKJJ4L"
+                        height="0" width="0" style="display:none;visibility:hidden"></iframe>`
+                }}
+            >
+
+            </noscript>
         </head>
         <body className="flex flex-col min-h-screen">
         <Header/>
