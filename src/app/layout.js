ff5fc47d356ce2d7ec70b94b80932437ea1082aa
alekskorolev3 <alekskorolev3@gmail.com>
2026-01-06 21:36:24 +0300

    Added variocube

--- src/app/layout.js
+++ src/app/layout.js
@@ -21,6 +21,24 @@ export default function RootLayout({children}) {
 
             <script type="text/javascript"
                     dangerouslySetInnerHTML={{
+                        __html: `(function (e, x, pe, r, i, me, nt
+            ){
+                e[i] = e[i] || function () {
+                    (e[i].a = e[i].a || []).push(arguments)
+                },
+                me=x.createElement(pe),me.async=1,me.src=r,nt=x.getElementsByTagName(pe)[0],me.addEventListener('error',function(){function cb(t){t=t[t.length-1],'function'==typeof t&&t({flags:{}})};Array.isArray(e[i].a)&&e[i].a.forEach(cb);e[i]=function(){cb(arguments)}}),nt.parentNode.insertBefore(me,nt)})
+                (window, document, 'script', 'https://abt.s3.yandex.net/expjs/latest/exp.js', 'ymab'
+            )
+            ;
+                ymab('metrika.106160339', 'init'/*, {clientFeatures}, {callback}*
+            /);`
+                    }}
+            >
+
+            </script>
+
+            <script type="text/javascript"
+                    dangerouslySetInnerHTML={{
                         __html: `(function(m,e,t,r,i,k,a){
                 m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
                 m[i].l=1*new Date();
