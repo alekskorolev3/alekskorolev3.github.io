b861ab9e218166970386fc197fc90ef521b328b4
alekskorolev3 <alekskorolev3@gmail.com>
2026-01-07 14:10:04 +0300

    Canonical for calculator

--- src/app/calculator/page.js
+++ src/app/calculator/page.js
@@ -1,10 +1,17 @@
 import { Suspense } from 'react'
 import CalculatorClient from './CalculatorClient'
+import Head from "next/head";
 
 export default function CalculatorPage() {
   return (
-      <Suspense fallback={<div className="p-8 text-gray-500">Загрузка калькулятора…</div>}>
-        <CalculatorClient />
-      </Suspense>
+      <>
+          <Head>
+              <link rel="canonical" href="https://flowauto.ru/calculator" />
+          </Head>
+
+          <Suspense fallback={<div className="p-8 text-gray-500">Загрузка калькулятора…</div>}>
+              <CalculatorClient />
+          </Suspense>
+      </>
   )
 }
