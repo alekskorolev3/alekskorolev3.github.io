b990aaef8134761c6ab744ea4125d151405e8172
alekskorolev3 <alekskorolev3@gmail.com>
2026-01-08 17:42:05 +0300

    Temporary removed google analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -31,22 +31,22 @@ export default function RootLayout({children}) {
         <Footer/>
         <ClientProviders/>
 
-        <script type="text/javascript"
-                dangerouslySetInnerHTML={{
-                    __html: `(function (e, x, pe, r, i, me, nt
-            ){
-                e[i] = e[i] || function () {
-                    (e[i].a = e[i].a || []).push(arguments)
-                },
-                me=x.createElement(pe),me.async=1,me.src=r,nt=x.getElementsByTagName(pe)[0],me.addEventListener('error',function(){function cb(t){t=t[t.length-1],'function'==typeof t&&t({flags:{}})};Array.isArray(e[i].a)&&e[i].a.forEach(cb);e[i]=function(){cb(arguments)}}),nt.parentNode.insertBefore(me,nt)})
-                (window, document, 'script', 'https://abt.s3.yandex.net/expjs/latest/exp.js', 'ymab'
-            )
-            ;
-                ymab('metrika.106160339', 'init');`
-                }}
-        >
+        {/*<script type="text/javascript"*/}
+        {/*        dangerouslySetInnerHTML={{*/}
+        {/*            __html: `(function (e, x, pe, r, i, me, nt*/}
+        {/*    ){*/}
+        {/*        e[i] = e[i] || function () {*/}
+        {/*            (e[i].a = e[i].a || []).push(arguments)*/}
+        {/*        },*/}
+        {/*        me=x.createElement(pe),me.async=1,me.src=r,nt=x.getElementsByTagName(pe)[0],me.addEventListener('error',function(){function cb(t){t=t[t.length-1],'function'==typeof t&&t({flags:{}})};Array.isArray(e[i].a)&&e[i].a.forEach(cb);e[i]=function(){cb(arguments)}}),nt.parentNode.insertBefore(me,nt)})*/}
+        {/*        (window, document, 'script', 'https://abt.s3.yandex.net/expjs/latest/exp.js', 'ymab'*/}
+        {/*    )*/}
+        {/*    ;*/}
+        {/*        ymab('metrika.106160339', 'init');`*/}
+        {/*        }}*/}
+        {/*>*/}
 
-        </script>
+        {/*</script>*/}
 
         <script type="text/javascript"
                 dangerouslySetInnerHTML={{
@@ -70,38 +70,38 @@ export default function RootLayout({children}) {
         </noscript>
 
 
-        <script type="text/javascript"
-                dangerouslySetInnerHTML={{
-                    __html: `(function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-WHFKJJ4L');`
-                }}
-        >
-        </script>
+        {/*<script type="text/javascript"*/}
+        {/*        dangerouslySetInnerHTML={{*/}
+        {/*            __html: `(function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start':*/}
+        {/*        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],*/}
+        {/*        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=*/}
+        {/*        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);*/}
+        {/*    })(window,document,'script','dataLayer','GTM-WHFKJJ4L');`*/}
+        {/*        }}*/}
+        {/*>*/}
+        {/*</script>*/}
 
-        <noscript
-            dangerouslySetInnerHTML={{
-                __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WHFKJJ4L"
-                        height="0" width="0" style="display:none;visibility:hidden"></iframe>`
-            }}
-        >
+        {/*<noscript*/}
+        {/*    dangerouslySetInnerHTML={{*/}
+        {/*        __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WHFKJJ4L"*/}
+        {/*                height="0" width="0" style="display:none;visibility:hidden"></iframe>`*/}
+        {/*    }}*/}
+        {/*>*/}
 
-        </noscript>
+        {/*</noscript>*/}
 
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-61FJTK1WT1"></script>
-        <script type="text/javascript"
-                dangerouslySetInnerHTML={{
-                    __html: `window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
+        {/*<script async src="https://www.googletagmanager.com/gtag/js?id=G-61FJTK1WT1"></script>*/}
+        {/*<script type="text/javascript"*/}
+        {/*        dangerouslySetInnerHTML={{*/}
+        {/*            __html: `window.dataLayer = window.dataLayer || [];*/}
+        {/*        function gtag(){dataLayer.push(arguments);}*/}
+        {/*        gtag('js', new Date());*/}
 
-                gtag('config', 'G-61FJTK1WT1');`
-                }}
-        >
+        {/*        gtag('config', 'G-61FJTK1WT1');`*/}
+        {/*        }}*/}
+        {/*>*/}
 
-        </script>
+        {/*</script>*/}
         </body>
         </html>
     );
