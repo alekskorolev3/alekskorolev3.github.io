8227830db4560603c53d7c8c113ad82197c4bcfe
alekskorolev3 <alekskorolev3@gmail.com>
2026-01-08 15:29:19 +0300

    Check for ////

---
+++ src/middleware.ts
@@ -0,0 +1,25 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+export function middleware(req: NextRequest) {
+    const url = req.nextUrl
+
+    // Убираем множественные слэши
+    if (url.pathname.includes('//')) {
+        const cleanPath = url.pathname.replace(/\/+/g, '/')
+
+        const newUrl = new URL(cleanPath, req.url)
+        return NextResponse.redirect(newUrl, 301)
+    }
+
+    // Явные дубли
+    if (
+        url.pathname === '/index.html' ||
+        url.pathname === '/index.php' ||
+        url.pathname === '/home'
+    ) {
+        return NextResponse.redirect(new URL('/', req.url), 301)
+    }
+
+    return NextResponse.next()
+}
